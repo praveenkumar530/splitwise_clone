f566b4cd2600ddf98405afa232ba924388ec815e
Praveen Kumar S <praveensk.530@gmail.com>
2025-07-28 01:14:33 +0530

    env update

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDOT1K1wP81oww4fWXvMss9VKrPX0zDg4Y",
-  authDomain: "splitwise-clone-272e5.firebaseapp.com",
-  projectId: "splitwise-clone-272e5",
-  storageBucket: "splitwise-clone-272e5.firebasestorage.app",
-  messagingSenderId: "1054280158995",
-  appId: "1:1054280158995:web:133e1108c627a86c25d4e6",
-  measurementId: "G-P9LYEXM9SG",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
